41f49dfd4eab55e510d44ea6e4f3c019bcf47917
Shahidh <muhammedshahid.k@gmail.com>
2015-01-28 17:50:08 +0530

    Edits

--- rcdn_server/rcdn_configs/settings.py
+++ rcdn_server/rcdn_configs/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'data',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -59,8 +60,10 @@ WSGI_APPLICATION = 'rcdn_configs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rcdn',
+        'USER': 'root',
+        'PASSWORD': 'omega',
     }
 }
 
